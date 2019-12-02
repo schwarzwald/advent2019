32b0299a56a952bd1b66380b6485de95f009f2f6
Pavol Dobrocka <pdb@whitestein.com>
2019-12-02 09:49:26 +0100

    Improve template generator

--- init.sh
+++ init.sh
@@ -9,11 +9,24 @@ fi
 if [ ! -f $d/part1.js ]; then
   touch $d/part1.js
   echo Created part1.js
+  echo "module.exports = input => {}" >> $d/part1.js
 fi
 
 if [ ! -f $d/part2.js ]; then
   touch $d/part2.js
   echo Created part2.js
+  echo "module.exports = input => {}" >> $d/part2.js
+fi
+
+if [ ! -f $d/test.js ]; then
+  touch $d/test.js
+  echo Created test.js
+  echo "const expect = require('expect.js');
+const part1 = require('./part1');
+const part2 = require('./part2');
+
+describe('Day $d: Part 1', () => {});
+describe('Day $d: Part 2', () => {});" >> $d/test.js
 fi
 
 if [ ! -f $d/input.txt ]; then
