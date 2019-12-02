aa38b2d61125f1237c3d024171435a7098941efb
bibo <bibaso@gmail.com>
2019-12-02 18:38:51 +0100

    Day 2 part 2

--- day02/part2.js
+++ day02/part2.js
@@ -1,3 +1,16 @@
 const int = require('./int');
 
-module.exports = input => {}
+module.exports = input => {
+  let instructions = input.split(',').map(Number);
+
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+      instructions[1] = noun;
+      instructions[2] = verb;
+
+      if (int(instructions)[0] == 19690720) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+}
--- day02/test.js
+++ day02/test.js
@@ -1,8 +1,7 @@
 const expect = require('expect.js');
 const int = require('./int');
-const part2 = require('./part2');
 
-describe('Day 02: Part 1', () => {
+describe('Day 02: Intcodes computer', () => {
   it('Should calculate correct output 1', () => {
     expect(int([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])).to.eql([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
   });
@@ -24,4 +23,3 @@ describe('Day 02: Part 1', () => {
     expect(int([1, 1, 1, 4, 99, 5, 6, 0, 99])).to.eql([30, 1, 1, 4, 2, 5, 6, 0, 99]);
   });
 });
-describe('Day 02: Part 2', () => {});
