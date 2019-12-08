743040a192b39ce1db567bb5124ea9d55b906735
bibo <bibaso@gmail.com>
2019-12-08 12:28:45 +0100

    Day 8: part 2

--- day08/part2.js
+++ day08/part2.js
@@ -1,1 +1,20 @@
-module.exports = input => {}
+module.exports = input => {
+  const width = 25;
+  const height = 6;
+  const size = width * height;
+
+  let layers = [];
+
+  for (let i = 0; i < input.length / size; i++) {
+    layers.push([...input.slice(i * size, (i + 1) * size)].map(Number));
+  }
+
+  let image = layers.reduce((r, l) => r.map((x, i) => x == 2 ? l[i] : x), [...new Array(size)].map(x => 2));
+  let rendered = '';
+
+  for (let y = 0; y < height; y++) {
+    rendered += image.slice(y * width, (y + 1) * width).map(c => c == 1 ? '#' : ' ').join('') + '\n';
+  }
+
+  return rendered;
+}
