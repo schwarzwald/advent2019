ac8e42396979f4f183b75d801750afa3d706fdeb
bibo <bibaso@gmail.com>
2019-12-17 22:05:13 +0100

    Day 16: part 2 solution

--- day16/part2.js
+++ day16/part2.js
@@ -1,93 +1,20 @@
-class Node {
-  constructor(value, next) {
-    this.value = value;
-    this.next = next;
-  }
-}
-
-const skip = (chain, n) => {
-  while (n-- && chain) {
-    chain = chain.next;
-  }
-  return chain;
-}
-
-const add = (chain, pos) => {
-  let skipping = true;
-  let mul = 1;
-  let res = 0;
-  let i = 0;
-
-  while (chain) {
-    if (skipping) {
-      chain = skip(chain, pos + 1);
-      skipping = false;
-    } else {
-      res += mul * chain.value;
-      i++;
-      chain = chain.next;
-
-      if (i == pos + 1) {
-        mul *= -1;
-        skipping = true;
-        i = 0;
-      }
-    }
-
-  }
-
-  return Math.abs(res % 10);
-}
-
-const phase = chain => {
-  let curr = chain;
-
-  let pos = 0;
-  let newChain = null;
-  let start = null;
-  while (curr) {
-    curr = curr.next;
-
-    if (!newChain) {
-      newChain = new Node(add(chain, pos));
-      start = newChain;
-    } else if (curr) {
-      start.next = new Node(add(chain, pos));
-      start = start.next;
-    }
-    pos++;
-  }
-  return newChain;
-}
-
-const toString = chain => {
-  let res = '';
-  while (chain) {
-    res += chain.value;
-    chain = chain.next;
-  }
-  return res;
-}
-
 module.exports = input => {
-  x = '12345678';
-  input = x;
+  let offset = +input.substr(0, 7);
 
-  for (let i = 0; i < 20; i++) {
-    let chain = new Node(0);
-    let start = chain;
-    x.split('').forEach(v => {
-      start.next = new Node(+v);
-      start = start.next;
-    });
-    x += input;
+  let numbers = input.split('').map(v => +v);
+  let vals = [];
 
-
-    for (let i = 0; i < 100; i++) {
-      chain = new Node(0, phase(chain));
-    }
-    console.log(toString(chain.next));
+  for (let i = offset; i < input.length * 10000; i++) {
+    vals.push(numbers[i % input.length]);
   }
 
-  //return str //.substr(0, 8); //phase('12345678', 0, 8); //phase('03036732577212944063491565474664', 303673, 8);
+  for (let i = 0; i < 100; i++) {
+    let sum = 0;
+    for (let j = vals.length - 1; j >= 0; j--) {
+      sum += vals[j];
+      vals[j] = sum % 10;
+    }
+  }
+
+  return vals.slice(0, 8).join('');
 }
--- day16/test.js
+++ day16/test.js
@@ -5,6 +5,10 @@ const part2 = require('./part2');
 describe('Day 16: Part 1', () => {
   it('Calculates the phase output', () => {
     expect(part1('80871224585914546619083218645595')).to.eql('24176176');
+  });
+});
+describe('Day 16: Part 2', () => {
+  it('Calculates the phase output for large value with offset', () => {
+    expect(part2('03036732577212944063491565474664')).to.eql('84462026')
   })
 });
-describe('Day 16: Part 2', () => { });
